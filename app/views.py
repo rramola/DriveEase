fa8a1a169776571d9b52d0d074b7ce5e3d4958cd
rramola <ramolaryan@gmail.com>
2024-05-05 15:05:23 -0500

    Worked on custom user creation form

--- app/views.py
+++ app/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpRequest, HttpResponse
 # Create your views here.
-
+from .forms import *
 def home(request):
     context ={}
     return render(request, "home.html",context)
 
 def registration_page(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect("home")
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
 
     context = {"form": form}
     return render(request, "registration.html", context)
